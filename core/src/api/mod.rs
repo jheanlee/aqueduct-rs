4b0a945b5a2b2c92276c748e74e1f569956364df
Jhean <jheanlee2008@gmail.com>
2026-04-05 15:46:32 +0800

    Added license

--- core/src/api/mod.rs
+++ core/src/api/mod.rs
@@ -13,17 +13,3 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
-
-@Entity()
-export class IpWhitelist {
-  @PrimaryKey()
-  id!: number;
-
-  @Property({ type: "inet" })
-  subnet!: string;
-
-  @Property({ type: "text" })
-  comment!: string;
-}
--- migration/src/mikro-orm.config.ts
+++ migration/src/mikro-orm.config.ts
@@ -1,8 +1,24 @@
-import 'dotenv/config'
-import {defineConfig} from '@mikro-orm/postgresql';
-import { TsMorphMetadataProvider } from '@mikro-orm/reflection';
-import {Migrator} from "@mikro-orm/migrations";
-import { SeedManager } from '@mikro-orm/seeder';
+/*
+ * Copyright 2026 Jhe-An Lee
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import "dotenv/config";
+import { defineConfig } from "@mikro-orm/postgresql";
+import { TsMorphMetadataProvider } from "@mikro-orm/reflection";
+import { Migrator } from "@mikro-orm/migrations";
+import { SeedManager } from "@mikro-orm/seeder";
 
 export default defineConfig({
   dbName: process.env.DB_NAME || "aqueduct-rs",
@@ -10,8 +26,8 @@ export default defineConfig({
   port: parseInt(process.env.DB_PORT || "5432"),
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  entities: ['dist/**/*.entity.js'],
-  entitiesTs: ['src/**/*.entity.ts'],
+  entities: ["dist/**/*.entity.js"],
+  entitiesTs: ["src/**/*.entity.ts"],
   metadataProvider: TsMorphMetadataProvider,
   extensions: [SeedManager, Migrator],
   debug: true,
